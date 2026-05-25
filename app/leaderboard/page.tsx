3c895b2bccf8f02b2c2f82b9b25329da55633815
William Halim <51820876+wiriim@users.noreply.github.com>
2026-05-25 16:22:01 +0700

    add leaderboard page

---
+++ app/leaderboard/page.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import type { User } from "../lib/definitions";
+import Top3 from "../ui/components/Leaderboard/Top3";
+
+import clsx from "clsx";
+import Link from "next/link";
+
+const backendServer = process.env.BACKEND_SERVER;
+
+export default async function Leaderboard() {
+  const users: User[] = await (await fetch(`${backendServer}/users`)).json();
+
+  const top3Users =
+    users.length > 3 ? users.slice(0, 3) : users.slice(0, users.length);
+
+  return (
+    <div className="flex flex-col items-center">
+      <Top3 users={top3Users} />
+
+      <div className="flex flex-col gap-3 w-[80vw] min-h-[400px] my-8 relative">
+        <div className="flex justify-between w-full items-start text-[32px]">
+          <h1>Rank</h1>
+          <h1>Name</h1>
+          <h1>Points</h1>
+        </div>
+        {users.map((user, i) => (
+          <div
+            key={user.id}
+            className={clsx(
+              "border border-2 w-full h-[108px] rounded-[10px] flex justify-between items-center p-8 text-[36px]",
+              {
+                "border-[#C8B912] border-3": i == 0,
+                "border-[#717171] border-3": i == 1,
+                "border-[#451C1C] border-3": i == 2,
+                "border-[#AAAAAA]": i > 2,
+              }
+            )}
+          >
+            <h2
+              className={clsx("font-bold text-[#AAAAAA]", {
+                "text-[#C8B912]": i == 0,
+                "text-[#717171]": i == 1,
+                "text-[#451C1C]": i == 2,
+              })}
+            >
+              {i + 1}
+            </h2>
+            <Link
+              href={`/profile/${user.username}`}
+              className="flex gap-2 items-center cursor-pointer hover:underline"
+            >
+              <div className="rounded-[100%] bg-[#E9E9E3] w-[65px] h-[65px]"></div>{" "}
+              {user.username}
+            </Link>
+            <h2>{user.point}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ app/ui/components/Leaderboard/Top3.tsx
@@ -0,0 +1,46 @@
+import { User } from "@/app/lib/definitions";
+import clsx from "clsx";
+
+export default function Top3({ users }: { users: User[] }) {
+  return (
+    <div className="flex flex-wrap gap-3 justify-around items-center w-[80vw] min-h-[400px] mt-8 relative">
+      {users.map((user, i) => (
+        <div
+          key={user.id}
+          className="flex flex-col w-[456px] h-[361px] rounded-[10px] border border-[#DEDEDE]"
+        >
+          <div
+            className={clsx(
+              "w-full h-2/5 rounded-t-[10px] font-bold relative z-0",
+              {
+                "bg-linear-to-r from-[#A9FFC7] to-[#FFFF8B]": i == 0,
+                "bg-linear-to-r from-[#FF98A7] to-[#EDEDED]": i == 1,
+                "bg-linear-to-r from-[#FFA9FB] to-[#71DCFF]": i == 2,
+              }
+            )}
+          >
+            {i == 0 ? (
+              <>
+                <h1 className="text-[96px] italic absolute right-20 -top-5">1</h1>
+                <h1 className="text-[36px] absolute right-8">st</h1>
+              </>
+            ) : i == 1 ? (
+                <>
+                <h1 className="text-[96px] italic absolute right-20 -top-5">2</h1>
+                <h1 className="text-[36px] absolute right-4">nd</h1>
+              </>
+            ) : (
+                <>
+                <h1 className="text-[96px] italic absolute right-20 -top-5">3</h1>
+                <h1 className="text-[36px] absolute right-8">rd</h1>
+              </>
+            )}
+          </div>
+          <div className="bg-[#E9E9E3] -mt-15 ml-10 rounded-[100%] w-[130px] h-[130px] z-1"></div>
+          <div className="text-[24px] ml-15 mt-3">{user.username}</div>
+          <div className="text-[24px] ml-15 mt-3">{user.point} Points</div>
+        </div>
+      ))}
+    </div>
+  );
+}
