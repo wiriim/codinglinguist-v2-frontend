fa552a6095b2716563c0aa50f622a98992c8d603
William Halim <51820876+wiriim@users.noreply.github.com>
2026-05-28 14:25:18 +0700

    add input & answer selection logic for questions

--- app/course/[name]/level/[levelNumber]/question/[questionNumber]/page.tsx
+++ app/course/[name]/level/[levelNumber]/question/[questionNumber]/page.tsx
@@ -1,31 +1,70 @@
+"use client";
 import type { Level, Question } from "@/app/lib/definitions";
 import Link from "next/link";
 import clsx from "clsx";
 import Image from "next/image";
+import { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
 
-const backendServer = process.env.BACKEND_SERVER;
+const backendServer = process.env.NEXT_PUBLIC_BACKEND_SERVER;
 
-export default async function Question(props: {
-  params: Promise<{
-    name: string;
-    levelNumber: string;
-    questionNumber: string;
-  }>;
-}) {
-  const params = await props.params;
+export default function Question() {
+  const params = useParams();
   const courseName = params.name;
-  const levelNumber = params.levelNumber;
-  const questionNumber = params.questionNumber;
+  const { levelNumber, questionNumber } = params;
 
-  const level: Level = await (
-    await fetch(`${backendServer}/courses/${courseName}/levels/${levelNumber}`)
-  ).json();
+  const [level, setLevel] = useState<null | Level>(null);
+  const [question, setQuestion] = useState<null | Question>(null);
+  const [answer, setAnswer] = useState<null | string>(null);
 
-  const question: Question = await (
-    await fetch(
-      `${backendServer}/courses/${courseName}/levels/${levelNumber}/questions/${questionNumber}`
-    )
-  ).json();
+  useEffect(() => {
+    console.log(`${backendServer}/courses/${courseName}/levels/${levelNumber}`);
+    async function fetchQuestion() {
+      const levelData: Level = await (
+        await fetch(
+          `${backendServer}/courses/${courseName}/levels/${levelNumber}`
+        )
+      ).json();
+      setLevel(levelData);
+
+      const questionData: Question = await (
+        await fetch(
+          `${backendServer}/courses/${courseName}/levels/${levelNumber}/questions/${questionNumber}`
+        )
+      ).json();
+      setQuestion(questionData);
+    }
+    fetchQuestion();
+  }, []);
+
+  useEffect(() => {
+    const choice = document.querySelectorAll(".question-choice");
+    for (const c of choice) {
+      if (c.textContent == answer)
+        (c as HTMLElement).style.border = "1px solid #00c753";
+    }
+
+    const input: null | HTMLInputElement =
+      document.querySelector(".question-input");
+    if (input && answer) {
+      input.value = answer;
+      input.focus();
+    }
+  }, [answer]);
+
+  function handleContainerClick(e: React.MouseEvent) {
+    const target = e.target as HTMLElement;
+    if (target.matches(".question-choice")) {
+      setAnswer(target.textContent);
+    }
+  }
+
+  function handleContainerChange(e: React.InputEvent) {
+    const target = e.target as HTMLInputElement;
+    if (target.matches(".question-input")) {
+      setAnswer(target.value);
+    }
+  }
 
   return (
     <div className="flex flex-col items-center w-full my-12">
@@ -43,7 +82,13 @@ export default async function Question(props: {
           />
           Back
         </Link>
-        <div dangerouslySetInnerHTML={{ __html: question.content }}></div>
+        {question && (
+          <div
+            onClick={handleContainerClick}
+            onInput={handleContainerChange}
+            dangerouslySetInnerHTML={{ __html: question.content }}
+          ></div>
+        )}
         <button className="border border-[#3E50DA] rounded-[10px] text-[#3E50DA] px-8 py-2 mt-8 cursor-pointer block ml-auto text-center hover:bg-[#3E50DA] hover:text-white">
           Submit
         </button>
@@ -57,18 +102,19 @@ export default async function Question(props: {
           >
             i
           </Link>
-          {level.questions.map((data, i) => (
-            <Link
-              href={`/course/${courseName}/level/${levelNumber}/question/${data.number}`}
-              key={data.number}
-              className={clsx("text-center rounded-[10px] px-5 py-2 ", {
-                "bg-white shadow-md": data.number == questionNumber,
-                "hover:bg-[#cacaca]": data.number != questionNumber,
-              })}
-            >
-              {data.number}
-            </Link>
-          ))}
+          {level &&
+            level.questions.map((data, i) => (
+              <Link
+                href={`/course/${courseName}/level/${levelNumber}/question/${data.number}`}
+                key={data.number}
+                className={clsx("text-center rounded-[10px] px-5 py-2 ", {
+                  "bg-white shadow-md": data.number == questionNumber,
+                  "hover:bg-[#cacaca]": data.number != questionNumber,
+                })}
+              >
+                {data.number}
+              </Link>
+            ))}
         </div>
       </div>
     </div>
