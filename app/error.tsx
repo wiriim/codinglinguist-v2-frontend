f613da736965c6a93d67a42b9a0a930a2ef2889b
William Halim <51820876+wiriim@users.noreply.github.com>
2026-06-18 17:34:28 +0700

    remove error msg in errors

--- app/error.tsx
+++ app/error.tsx
@@ -19,7 +19,6 @@ export default function Error({
       <h2 className="text-center">
         Something went wrong! Please try again later.
       </h2>
-      <h2 className="text-center my-5 text-red-500">{error.message}</h2>
       <button
         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400 cursor-pointer"
         onClick={() => reset()}
