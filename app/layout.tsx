71542ac55cff50ea55185328bc8e37c60812474b
William Halim <51820876+wiriim@users.noreply.github.com>
2026-06-25 17:42:30 +0700

    add top loader to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { montserrat } from "./ui/fonts";
 import "./globals.css";
 import Navbar from "./ui/components/Navbar/Navbar";
+import NextTopLoader from "nextjs-toploader";
 
 export const metadata: Metadata = {
   title: "CodingLinguist",
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${montserrat.className} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
+        <NextTopLoader />
         <Navbar />
         {children}
       </body>
